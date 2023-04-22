eb1fef1c94fc77ccccb32db4a32f5eea5fc3b2a7
Seungmin Nam <nhjbest22@g.skku.edu>
2023-04-22 15:13:43 +0000

    txt를 통한 채팅 데이터 저장 및 시각화

--- app.js
+++ app.js
@@ -6,17 +6,29 @@ const {Server} = require('socket.io');
 const io = new Server(server);
 const path = require('path');
 const port = 8080
-
-
-
+const filePath = 'chatdata.txt';
+const fs = require('fs');
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+// let data = fs.readFileSync(filePath, "utf8");
+// data.split('\n').forEach((msg)=>{
+//     console.log(msg);
+// })
+
 io.on('connection', (socket)=>{
-    console.log("new user connect!");
+    let chatData = fs.readFileSync(filePath, "utf8");
+    console.log("user user come!");
+    socket.emit('new user', chatData);
+
     socket.on('disconnect', ()=>{
         console.log('user disconnected!');
+    })
+    socket.on('chat message', (msg)=>{
+        console.log("chat message!");
+        fs.appendFileSync(filePath, '\n'+msg);
+        io.emit('chat message', msg);
     })
 })
 
